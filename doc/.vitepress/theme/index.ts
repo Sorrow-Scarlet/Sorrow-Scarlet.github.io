d2f688aee6fda386ed57d6a0d602a435418d4941
Sorrow-Work <61646954+Sorrow-Scarlet@users.noreply.github.com>
2025-07-09 10:14:12 +0800

    铸币了，没给node module添加gitignore，没有npm经验，经验+1 git用的也不算太熟练，疯狂刷提交了属于是

--- doc/.vitepress/theme/index.ts
+++ doc/.vitepress/theme/index.ts
@@ -1,7 +1,20 @@
 import DefaultTheme from "vitepress/theme";
 import "./style/index.css"; //引入自定义的样式
+import { inBrowser } from "vitepress";
+import busuanzi from "busuanzi.pure.js";
+import DataPanel from "./components/DataPanel.vue";
+
+
 
 export default {
   extends: DefaultTheme,
-  // ...DefaultTheme, //或者这样写也可
+  enhanceApp({ app, router }) {
+    app.component("DataPanel", DataPanel);//注册全局组件
+    if (inBrowser) {
+      router.onAfterRouteChanged = () => {
+        busuanzi.fetch();
+      };
+    }
+  },
+
 };
