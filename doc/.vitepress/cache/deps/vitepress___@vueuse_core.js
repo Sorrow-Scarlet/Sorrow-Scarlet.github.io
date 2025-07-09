2a1c7ac095df602ec1b6c4b5398b11594f4e35a6
Sorrow-Work <61646954+Sorrow-Scarlet@users.noreply.github.com>
2025-07-09 08:53:24 +0800

    重新安排文件夹结构....不该乱改的，以及vite这个插件需要改一下路径，不然就默认在doc外面的路径搞事

--- doc/.vitepress/cache/deps/vitepress___@vueuse_core.js
+++ doc/.vitepress/cache/deps/vitepress___@vueuse_core.js
@@ -35,7 +35,7 @@ import {
   unref,
   watch,
   watchEffect
-} from "./chunk-W6V53FDX.js";
+} from "./chunk-DDXJJ377.js";
 
 // node_modules/@vueuse/shared/index.mjs
 function computedEager(fn, options) {
--- doc/.vitepress/cache/deps/vue.js
+++ doc/.vitepress/cache/deps/vue.js
@@ -168,7 +168,7 @@ import {
   withMemo,
   withModifiers,
   withScopeId
-} from "./chunk-W6V53FDX.js";
+} from "./chunk-DDXJJ377.js";
 export {
   BaseTransition,
   BaseTransitionPropsValidators,
