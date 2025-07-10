551f9d65adff33b6e3db1f6b2f6b55266d51824e
Sorrow-Work <61646954+Sorrow-Scarlet@users.noreply.github.com>
2025-07-10 14:00:36 +0800

    更新测试页，方便之后调用

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,14 +1,35 @@
 import { defineConfig } from 'vitepress'
+import { generateSidebar } from "vitepress-sidebar";  
+
+const vitepressSidebarOptions = {  
+  documentRootPath: '/docs',  
+  collapsed: false, //折叠组关闭
+  collapseDepth: 2, //折叠组2级菜单
+  removePrefixAfterOrdering: true, //删除前缀，必须与prefixSeparator一起使用
+  prefixSeparator: "_", //删除前缀的符号
+};  
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+    markdown:{
+      // 代码块行号显示
+    lineNumbers: true,
+      // 图片懒加载
+    image: {
+      lazyLoading: true,
+       // 代码框内复制按钮的 title 提示
+      },
+      codeCopyButtonTitle: '复制',
+      },
   title: "忧的主页",
   description: "主页",
+  srcDir: "",//等下需要新建一个目录，统一放这里
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+        sidebar: generateSidebar(vitepressSidebarOptions),  
     nav: [
       { text: '首页', link: '/' },
-      { text: '空调', link: '/占位符' },
+      { text: '空调', link: '/airconditioner' },
       { text: 'Examples', link: '/markdown-examples' }
     ],
 
@@ -24,6 +45,15 @@ export default defineConfig({
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/Sorrow-Scarlet' },
+      {
+        icon: 
+        {
+          svg: '',
+        },
+        link: "https://bilibili.com"
+      }
     ]
   }
+
+  
 })
