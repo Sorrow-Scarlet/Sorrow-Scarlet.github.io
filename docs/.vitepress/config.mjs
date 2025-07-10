33392ecd94df322123e8db1a9def784d82b53850
Sorrow-Work <61646954+Sorrow-Scarlet@users.noreply.github.com>
2025-07-10 13:34:52 +0800

    安装了空调，添加了测试页和建设页，加入了remixicon的bilibili svg图标，准备建设社媒链接

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,14 +1,25 @@
 import { defineConfig } from 'vitepress'
+import { generateSidebar } from "vitepress-sidebar";  
+
+const vitepressSidebarOptions = {  
+  documentRootPath: '/docs',  
+  collapsed: false, //折叠组关闭
+  collapseDepth: 2, //折叠组2级菜单
+  removePrefixAfterOrdering: true, //删除前缀，必须与prefixSeparator一起使用
+  prefixSeparator: "_", //删除前缀的符号
+};  
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "忧的主页",
   description: "主页",
+  srcDir: "",//等下需要新建一个目录，统一放这里
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+        sidebar: generateSidebar(vitepressSidebarOptions),  
     nav: [
       { text: '首页', link: '/' },
-      { text: '空调', link: '/占位符' },
+      { text: '空调', link: '/airconditioner' },
       { text: 'Examples', link: '/markdown-examples' }
     ],
 
